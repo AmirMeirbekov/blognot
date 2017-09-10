d7b1d53094265e2bad7464121508c81fcf9772c9
Amir Meirbekov <abaurovich@gmail.com>
2017-09-11 00:01:26 +0300

    [+] Adds first test tests

---
+++ tests/test_1.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+import pytest
+
+
+@pytest.fixture
+def response():
+    """
+    Sample pytest fixture.
+    See more at: http://doc.pytest.org/en/latest/fixture.html
+    """
+    import requests
+    return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
+
+
+def test_content(response):
+    pass
